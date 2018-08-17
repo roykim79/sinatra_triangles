6b064fb52e561047b59da51edbff05cbede4f9e2
Roy Kim <rykim79@gmail.com>
2018-08-17 13:28:22 -0400

    add tests to return "NOT a triangle" for triangles that are not triangles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,12 +6,16 @@ class Triangle
   end
 
   def type
-    if (@length1 == @length2) & (@length2 == @length3)
-      'equilateral'
-    elsif ((@length1 == @length2) & (@length1 != @length3)) | ((@length1 == @length3) & (@length1 != @length2)) | ((@length2 == @length3) & (@length2 != @length1))
-      'isosceles'
-    elsif (@length1 != @length2) & (@length1 != @length3) & (@length2 != @length3)
-      'scalene'
+    if (@length1 + @length2 <= @length3) | (@length1 + @length3 <= @length2) | (@length2 + @length3 <= @length1)
+      'NOT a triangle'
+    else
+      if (@length1 == @length2) & (@length2 == @length3)
+        'equilateral'
+      elsif ((@length1 == @length2) & (@length1 != @length3)) | ((@length1 == @length3) & (@length1 != @length2)) | ((@length2 == @length3) & (@length2 != @length1))
+        'isosceles'
+      elsif (@length1 != @length2) & (@length1 != @length3) & (@length2 != @length3)
+        'scalene'
+      end
     end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -27,5 +27,20 @@ describe Triangle do
       triangle = Triangle.new(15, 10, 20)
       expect(triangle.type()).to eq('scalene')
     end
+
+    it "returns 'NOT a triangle' if the sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side" do
+      triangle = Triangle.new(15, 10, 30)
+      expect(triangle.type()).to eq('NOT a triangle')
+    end
+
+    it "returns 'NOT a triangle' if the sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side" do
+      triangle = Triangle.new(30, 15, 10)
+      expect(triangle.type()).to eq('NOT a triangle')
+    end
+
+    it "returns 'NOT a triangle' if the sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side" do
+      triangle = Triangle.new(15, 30, 10)
+      expect(triangle.type()).to eq('NOT a triangle')
+    end
   end
 end
